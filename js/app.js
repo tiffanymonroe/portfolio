f350db88cd32b7897323d7a72eb2d6388ca6a869
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-24 18:04:16 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const app = angular.module('website', []);
+
+
+app.controller('MainController', ['$http', function($http){
+  const controller = this;
+
+  //show-hide logic, from Karolin
+  this.about = false;
+  this.contact = false;
+
+  this.toggleAbout = function(){
+    this.about = !this.about;
+    if (this.contact === true){
+      this.contact = false;
+    }
+  };
+
+
+  this.toggleContact = function(){
+    this.contact = !this.contact;
+    if (this.about === true){
+      this.about = false;
+    }
+  };
+
+
+
+
+
+}]); //end of controller
