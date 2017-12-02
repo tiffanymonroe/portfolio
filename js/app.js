409f641778e7ec670dbf7a5432be9c7d6f39d1ca
Tiffany Monroe <hello@tiffanymonroe.com>
2017-12-01 22:50:24 -0800

    modals

--- js/app.js
+++ js/app.js
@@ -5,21 +5,21 @@ app.controller('MainController', ['$http', function($http){
   const controller = this;
 
   //show-hide logic, from Karolin
-  this.about = false;
-  this.contact = false;
-
-  this.toggleAbout = function(){
-    this.about = !this.about;
-    if (this.contact === true){
-      this.contact = false;
-    }
-  };
-
-
-  this.toggleContact = function(){
-    this.contact = !this.contact;
-    if (this.about === true){
-      this.about = false;
+  // this.about = false;
+  // this.contact = false;
+  //
+  // this.toggleAbout = function(){
+  //   this.about = !this.about;
+  //   if (this.contact === true){
+  //     this.contact = false;
+  //   }
+  // };
+  //
+  //
+  // this.toggleContact = function(){
+  //   this.contact = !this.contact;
+  //   if (this.about === true){
+  //     this.about = false;
     }
   };
 
