fe60d5cd9cc5261ca60d34586921d33e24910257
Tiffany Monroe <hello@tiffanymonroe.com>
2017-12-01 23:59:17 -0800

    refactored code

--- js/app.js
+++ js/app.js
@@ -5,25 +5,12 @@ app.controller('MainController', ['$http', function($http){
   const controller = this;
 
   //show-hide logic, from Karolin
-  // this.about = false;
-  // this.contact = false;
-  //
-  // this.toggleAbout = function(){
-  //   this.about = !this.about;
-  //   if (this.contact === true){
-  //     this.contact = false;
-  //   }
-  // };
-  //
-  //
-  // this.toggleContact = function(){
-  //   this.contact = !this.contact;
-  //   if (this.about === true){
-  //     this.about = false;
-    }
+  this.portfolio = false;
+
+
+  this.showPortfolio = function(){
+    this.portfolio = !this.portfolio;
   };
-
-
 
 
 
