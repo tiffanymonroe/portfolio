95dc178259a18b6e0dad2a73e49e7e8fe166dbe6
Tiffany Monroe <hello@tiffanymonroe.com>
2017-12-02 00:44:48 -0800

    portfolio

--- js/app.js
+++ js/app.js
@@ -5,13 +5,27 @@ app.controller('MainController', ['$http', function($http){
   const controller = this;
 
   //show-hide logic, from Karolin
+  this.main = true;
+  this.title = true;
   this.portfolio = false;
 
 
   this.showPortfolio = function(){
-    this.portfolio = !this.portfolio;
+    this.portfolio = true;
+    this.main = false;
+    this.title = false;
+
   };
 
+  this.hidePortfolio = function(){
+      this.main = true;
+      this.title = true;
+      this.portfolio = false;
+  };
 
+  if(this.portfolio === false){
+    this.main = true;
+    this.title = true;
+  }
 
 }]); //end of controller
