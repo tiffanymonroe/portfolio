2a474ed640a75629cd14b456a2d8dd7ca0fb2137
Tiffany Monroe <hello@tiffanymonroe.com>
2018-01-17 17:38:18 -0800

    DNC

--- js/app.js
+++ js/app.js
@@ -29,3 +29,30 @@ app.controller('MainController', ['$http', function($http){
   }
 
 }]); //end of controller
+
+app.controller('DncController', ['$http', function($http){
+  const controller = this;
+
+//Hide sections
+  this.qualifications = true;
+  this.background = true;
+  this.mission = true;
+  this.micDrop = true;
+
+//Activate buttons
+  this.showQualifications = function(){
+    this.qualifications = false;
+  }
+
+  this.showBackground = function(){
+    this.background = false;
+  }
+
+  this.showMission = function(){
+    this.mission = false;
+  }
+
+  this.showMicDrop = function(){
+    this.micDrop = false;
+  }
+}]); //end of DNC Controller
