967743770ed46b8cc8ccc69f83f337b154556f05
Tiffany Monroe <hello@tiffanymonroe.com>
2019-06-30 15:18:52 -0700

    organize using work container

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('accio js!');
+
+const workContainer = document.getElementsByClassName('work-container');
+const upworthyContainer = document.getElementsByClassName('upworthy-container');
+const swaayContainer = document.getElementsByClassName('swaay-container');
+const upworthyLogo = document.querySelector('upworthy-logo');
+const swaayLogo = document.getElementsByClassName('swaay-logo');
+
+
+
+const toggleContainer = () => {
+  workContainer.style.display('none');
+}
+
+console.log(upworthyLogo);
